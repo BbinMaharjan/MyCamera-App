9bfb2c1de445b1b96555bd766052121ad67c8422
BbinMaharjan <bbinmaharjan@gmail.com>
2021-04-07 17:19:49 +0545

    camera

--- App.js
+++ App.js
@@ -1,111 +1,92 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+import React, {useState} from 'react';
+import {Button, Root, ActionSheet, Text} from 'native-base';
+import {View, StyleSheet, Image} from 'react-native';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+const App = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <Root>
+      <View style={styles.screen}>
+        <View>
+          <Button
+            success
+            onPress={() => {
+              ActionSheet.show(
+                {
+                  options: [
+                    {
+                      text: 'Gallery',
+                      icon: 'md-images',
+                      iconColor: 'blue',
+                    },
+                    {
+                      text: 'Camera',
+                      icon: 'md-camera',
+                      iconColor: 'green',
+                    },
+                    {
+                      text: 'Cancle',
+                      icon: 'close',
+                      iconColor: 'red',
+                    },
+                  ],
+                  title: 'Chose Source',
+                  cancelButtonIndex: 2,
+                },
+                selectedIndex => {
+                  if (selectedIndex == 0) {
+                    launchImageLibrary(
+                      {
+                        quality: 1,
+                        maxWidth: 200,
+                        maxHeight: 200,
+                      },
+                      response => {
+                        setSelectedImage({uri: response.uri});
+                      },
+                    );
+                  } else if (selectedIndex == 1) {
+                    launchCamera(
+                      {
+                        quality: 1,
+                        maxHeight: 200,
+                        maxWidth: 200,
+                        includeBase64: true,
+                      },
+                      response => {
+                        setSelectedImage({uri: response.uri});
+                      },
+                    );
+                  }
+                },
+              );
+            }}>
+            <Text>Select Image</Text>
+          </Button>
+          <View style={styles.imageContiner}>
+            <Image source={selectedImage} style={styles.image} />
+          </View>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+      </View>
+    </Root>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  screen: {
+    flex: 1,
+    marginTop: 50,
+    paddingHorizontal: 25,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  imageContiner: {
+    marginTop: 20,
+    height: 200,
+    width: 200,
+    backgroundColor: 'skyblue',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  image: {
+    width: '100%',
+    height: '100%',
   },
 });
 
